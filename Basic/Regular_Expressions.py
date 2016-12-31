efec9f17ac024b6ccab73ccb5b84232080506f59
sunbohit <a824834@163.com>
2016-12-31 16:36:42 +0800

    更新正则表达式

--- Basic/Regular_Expressions.py
+++ Basic/Regular_Expressions.py
@@ -79,4 +79,53 @@ m.span(2): (6, 11)
 m.expand(r'\g \g\g'): world hello!
 '''
  
+# 将正则表达式编译成Pattern对象
+pattern = re.compile(r'world')
+# 使用search()查找匹配的子串，不存在能匹配的子串时将返回None
+# 这个例子中使用match()无法成功匹配
+match = re.search(pattern,'hello world!')
+if match:
+    # 使用Match获得分组信息
+    print(match.group())
+'''
+world
+'''
+
+pattern = re.compile(r'\d+')
+print(re.split(pattern,'one1two2three3four4'))
+'''
+['one', 'two', 'three', 'four', '']
+'''
+
+pattern = re.compile(r'\d+')
+print(re.findall(pattern,'one1two2three3four4'))
+'''
+['1', '2', '3', '4']
+'''
+ 
+pattern = re.compile(r'\d+')
+for m in re.finditer(pattern,'one1two2three3four4'):
+    print(m.group(),",")
+'''
+1 ,
+2 ,
+3 ,
+4 ,
+'''
+
+pattern = re.compile(r'(\w+) (\w+)')
+s = 'i say, hello world!'
+ 
+print(re.sub(pattern,r'\2 \1', s))
+'''
+say i, world hello!
+'''
+ 
+def func(m):
+    return m.group(1).title() + ' ' + m.group(2).title()
+'''
+I Say, Hello World!
+'''
+ 
+print(re.sub(pattern,func, s))
 

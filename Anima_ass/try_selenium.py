fe83e57af6c6acfc45a7da4530a20a00063f86c6
sunbohit <a824834@163.com>
2017-04-07 22:00:15 +0800

    Anima

--- Anima_ass/try_selenium.py
+++ Anima_ass/try_selenium.py
@@ -1,9 +1,18 @@
 from selenium import webdriver
+import time
 
-driver = webdriver.Chrome()
-driver.get('http://subhd.com/ar0/327068')
+file_path = './url.txt'
+infile = open(file_path,'r')
 
-element = driver.find_element_by_id("down")
-element.click()
-print(element.text)
-driver.quit()
+for line in infile.readlines():
+	line = line.strip()
+	print(line)
+	download(line)
+def download(urlline)
+	driver = webdriver.Chrome()
+	driver.get(urlline)
+	element = driver.find_element_by_id("down")
+	element.click()
+	print(element.text)
+	time.sleep(100)
+	driver.quit()

e6d6d39b72cfdd857a18e4b202d076347febb1f9
sunbohit <a824834@163.com>
2017-04-08 10:01:24 +0800

    file

--- Anima_ass/try_selenium.py
+++ Anima_ass/try_selenium.py
@@ -1,18 +1,21 @@
 from selenium import webdriver
 import time
 
-file_path = './url.txt'
+file_path = './ass/url.txt'
 infile = open(file_path,'r')
 
-for line in infile.readlines():
-	line = line.strip()
-	print(line)
-	download(line)
-def download(urlline)
+
+def download(urlline):
 	driver = webdriver.Chrome()
 	driver.get(urlline)
 	element = driver.find_element_by_id("down")
 	element.click()
 	print(element.text)
-	time.sleep(100)
+	time.sleep(3000)
 	driver.quit()
+count = 1
+for line in infile.readlines():
+	line = line.strip()
+	print(str(count)+" : "+line)
+	download(line)
+	count+=1
